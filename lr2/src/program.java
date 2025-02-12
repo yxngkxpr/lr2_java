e076d1e1d52231d30f4ea7e1f7aa9628692e8587
yxngkxpr <164497219+yxngkxpr@users.noreply.github.com>
2025-02-12 14:15:20 +0300

    Add files via upload

---
+++ lr2/src/program.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import static java.lang.Math.pow;
+
+public class program {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+        final double PI = 3.14;
+        System.out.print("\t\tЗАДАНИЕ 1\n");
+        System.out.print("Введите x: ");
+        int x = in.nextInt();
+        System.out.print("Введите у: ");
+        int y = in.nextInt();
+        double num1 = 2 * (1 + pow(x,2) + 3 * y) - 12 * pow(x,2) * y + PI;
+        System.out.printf("Результат = %.2f\n", num1);
+        System.out.print("\t\tЗАДАНИЕ 2\n");
+    System.out.print("Введите внешний радиус кольца: ");
+        int r1 = in.nextInt();
+    System.out.print("Введите внутренний радиус кольца: ");
+        int r2 = in.nextInt();
+        double num2 = PI * (r1 - r2);
+    System.out.printf("Площадь кольца = %.2f", num2);
+    in.close();
+}
+
+
+
+
+}
